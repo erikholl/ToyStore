6b6f25beac3d31e08d6d1cb6679f5e7d9c78ceda
erikh <91531357+erikholl@users.noreply.github.com>
2022-05-19 10:58:39 +0200

    price and link added

--- src/main/java/be/intecbrussels/data/Animal.java
+++ src/main/java/be/intecbrussels/data/Animal.java
@@ -17,15 +17,20 @@ public class Animal {
     @Column(name="country")
     private String country;
 
-//    @Column(name="price")
-//    private int price;
+    @Column(name="price")
+    private int price;
+
+    @Column(name="link")
+    private String link;
 
     public Animal() {
     }
 
-    public Animal(String name, String country) {
+    public Animal(String name, String country, int price, String link) {
         this.name = name;
         this.country = country;
+        this.price = price;
+        this.link = link;
     }
 
     public int getId() {
@@ -50,5 +55,32 @@ public class Animal {
 
     public void setCountry(String country) {
         this.country = country;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", country='" + country + '\'' +
+                ", price=" + price +
+                ", link='" + link + '\'' +
+                '}';
     }
 }
